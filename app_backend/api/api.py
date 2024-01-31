00e1bf75a618d169d027d32348ed99ee3b363d11
ArianFotouhi <fotouhiarian@gmail.com>
2024-01-31 12:05:29 -0500

    v1.4.7

--- app_backend/api/api.py
+++ app_backend/api/api.py
@@ -1,4 +1,5 @@
-from ninja import NinjaAPI
+from ninja_extra import NinjaExtraAPI, api_controller, route
+
 from api.models import Device, Location
 from api.schemas import (
     DeviceSchema,
@@ -10,18 +11,46 @@ from api.schemas import (
 from django.shortcuts import get_object_or_404
 
 
-app = NinjaAPI()
+app = NinjaExtraAPI()
+
+@api_controller('/devices', tags=['Devices'], permissions=[])
+class DeviceController:
+
+    @route.get("/", response=list[DeviceSchema])
+    def get_devices(self):
+        return Device.objects.all()
 
 
-@app.get("devices/", response=list[DeviceSchema])
-def get_devices(request):
-    return Device.objects.all()
+    @route.post("/", response={200: DeviceSchema, 404: Error})
+    def create_device(self, device: DeviceCreateSchema):
+        if device.location_id:
+            # we have a location ID
+            location_exists = Location.objects.filter(id=device.location_id).exists()
+            if not location_exists:
+                return 404, {"message": "Location not found"}
+
+        device_data = device.model_dump()
+        device_model = Device.objects.create(**device_data)
+        return device_model
+    
+    
+    @route.get("/{slug}/", response=DeviceSchema)
+    def get_device(self, slug: str):
+        device = get_object_or_404(Device, slug=slug)
+        return device
 
 
-@app.get("devices/{slug}/", response=DeviceSchema)
-def get_device(request, slug: str):
-    device = get_object_or_404(Device, slug=slug)
-    return device
+    @route.post('/{device_slug}/set-location/', response=DeviceSchema)
+    def update_device_location(self, device_slug, location: DeviceLocationPatch):
+        device = get_object_or_404(Device, slug = device_slug)
+        if location.location_id:
+            location = get_object_or_404(Location, id=location.location_id)
+            device.location = location
+        else:
+            device.location = None
+
+        device.save()
+        return device
 
 
 @app.get("locations/", response=list[LocationSchema])
@@ -29,28 +58,8 @@ def get_locations(request):
     return Location.objects.all()
 
 
-@app.post("devices/", response={200: DeviceSchema, 404: Error})
-def create_device(request, device: DeviceCreateSchema):
-    if device.location_id:
-        # we have a location ID
-        location_exists = Location.objects.filter(id=device.location_id).exists()
-        if not location_exists:
-            return 404, {"message": "Location not found"}
-
-    device_data = device.model_dump()
-    device_model = Device.objects.create(**device_data)
-    return device_model
-
-@app.post('devices/{device_slug}/set-location/', response=DeviceSchema)
-def update_device_location(request, device_slug, location: DeviceLocationPatch):
-    device = get_object_or_404(Device, slug = device_slug)
-    if location.location_id:
-        location = get_object_or_404(Location, id=location.location_id)
-        device.location = location
-    else:
-        device.location = None
-
-    device.save()
-    return device
 
 
+
+
+app.register_controllers(DeviceController)
